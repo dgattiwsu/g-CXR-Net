1ff79c2d4a7017cf8b5356262bf1a5e7a9d20a43
Domenico Gatti <dgatti@med.wayne.edu>
2021-05-17 19:05:40 -0400

    Delete CXR_Net_icons directory

--- CXR_Net_icons/make_shortcut_with_arguments.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from pyshortcuts import make_shortcut
-make_shortcut('/Users/dgatti/Documents/COVID19/CXR-Net_for_github/CXR-Net/run_CXR-Net/CXR_Net_gui.py --xdir /Users/dgatti/Documents/COVID19/CXR-Net_for_github/CXR-Net/run_CXR-Net',
-              name='g_CXR_Net',
-              description='CXR_Net gui',
-              folder=None,
-              icon='/Users/dgatti/Desktop/CXR_Net_icons/CXR_NET_ICON.icns',
-              terminal=True, desktop=True, startmenu=True,
-              executable='/Users/dgatti/venv_jupyter/bin/python3.8')
